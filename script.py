4a27f6ff7f1681b246b6654724b918907810206a
pauloh-alc <pauloalencar.aluno@gmail.com>
2022-10-31 21:49:55 -0300

    Adicionando libs que serão utilizadas
    * libs utilizadas:
    1. tabula
    2. zipfile
    3. csv

--- script.py
+++ script.py
@@ -1,1 +1,12 @@
 #!/usr/bin/env python
+
+import tabula as tb
+import zipfile as zf
+import csv
+
+# Used libs:
+"""
+    *tabula : tabula-py enables you to extract tables from a PDF into a DataFrame, or a JSON.
+    *csv    : the csv library provides functionality to both read from and write to CSV files.
+    *zipfile: the ZIP file format is a common archive and compression standar.
+"""
