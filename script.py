f28dd7a7aa4e0a07ae5502d3314f881e9d02e144
pauloh-alc <pauloalencar.aluno@gmail.com>
2022-10-31 22:19:53 -0300

    Criando função que vai manipular os dados e colocar no file CSV
    1. A função primeiramente adiciona o nome das colunas no arquivo csv;
    2. A lista nomeada row armazenará uma linha completa de uma tabela;
    3. O primeiro <for> navega por todas as tabelas do PDF;
    4. O segundo <for> navega pelas linhas de cada tabela;
    5. É utilizado a função fillna para remove os valores NaN das listas.

--- script.py
+++ script.py
@@ -25,3 +25,22 @@ QTD_TABLES = LAST_PAGE - FIRST_PAGE + 1
 
 # Read PDF into list of DataFrames
 list_dfs = tb.read_pdf(FILE_NAME_PDF+'.pdf', pages=str(FIRST_PAGE) + '-' + str(LAST_PAGE), lattice=True)
+
+# Manipulating the .csv file
+def write_to_csv_file():
+    with open('./'+FILE_NAME_CSV+'.csv', 'w') as csvfile:
+        # Write the columns name
+        csv.writer(csvfile, delimiter=',').writerow(list(list_dfs[0].columns))
+        # Stores each table row in a list
+        row = []
+
+        # Scroll through pdf tables
+        for i in range(0,QTD_TABLES):
+            qtd_rows = len(list_dfs[i].index)
+            
+            # Traverse the table rows
+            for j in range(0,qtd_rows):
+                list_dfs[i].fillna(value='', inplace = True)        # replace nan values with blank values.
+                row = list(list_dfs[i].iloc[j])                     # selecting line from DataFrame.
+                #replace_column_data(row, i, j)                     Replaces the short data in the OD and AMB columns with their descriptions.
+                #csv.writer(csvfile, delimiter=',').writerow(row)   write to csv file
